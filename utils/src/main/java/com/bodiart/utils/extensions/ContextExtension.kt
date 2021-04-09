5a92782286626a54ca58c6ddab8e3a35aa090fb1
Bohdan <b.hochachko@ideil.com>
2021-04-09 16:57:55 +0300

    Configured KDoc

--- utils/src/main/java/com/bodiart/utils/extensions/ContextExtension.kt
+++ utils/src/main/java/com/bodiart/utils/extensions/ContextExtension.kt
@@ -4,4 +4,8 @@ import android.content.Context
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 
+/**
+ * Get color compat
+ * @param colorResId - color resource id
+ */
 fun Context.getColorCompat(@ColorRes colorResId: Int) = ContextCompat.getColor(this, colorResId)
