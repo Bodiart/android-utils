0566f86a297b3dac58095078df047e8f62b7dc2b
b.hochachko <b.hochachko@ideil.com>
2022-01-12 15:21:13 +0200

    Activity extensions: fixes

--- utils/src/main/java/io/github/bodiart/utils/extensions/ActivityExtension.kt
+++ utils/src/main/java/io/github/bodiart/utils/extensions/ActivityExtension.kt
@@ -40,9 +40,9 @@ fun Activity.toast(textComb: TextComb, duration: Int = Toast.LENGTH_SHORT) =
  */
 @Suppress("DEPRECATION")
 fun Activity.showContentBehindStatusBar() {
+    window?.statusBarColor = Color.TRANSPARENT
     window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-    window?.statusBarColor = Color.TRANSPARENT
 }
 
 /**
